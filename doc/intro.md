cbcc76d990e76794f29966d93182e3544615eb93
Denis Labaye <denis.labaye@gmail.com>
2013-02-18 07:21:07 +0100

    initial

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to euler-denlab
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/euler_denlab/core_test.clj
@@ -0,0 +1,7 @@
+(ns euler-denlab.core-test
+  (:use clojure.test
+        euler-denlab.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
