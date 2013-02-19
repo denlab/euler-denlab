7f91feb84c8c232f62fa3c383fed4aaefefec727
Denis Labaye <denis.labaye@gmail.com>
2013-02-19 18:18:11 +0100

    Euler26: A fn to calculate an infinite number of decimals of 1/n

--- src/euler_denlab/doing/p26_reciprocal_cycles.clj
+++ src/euler_denlab/doing/p26_reciprocal_cycles.clj
@@ -19,11 +19,11 @@
 
 ;; Reciprocal cycles
 ;; Problem 26
-
+;;
 ;; A unit fraction contains 1 in the numerator. The decimal
 ;; representation of the unit fractions with denominators 2 to 10 are
 ;; given:
-
+;;
 ;; 1/2	= 	0.5
 ;; 1/3	= 	0.(3)
 ;; 1/4	= 	0.25
@@ -33,13 +33,37 @@
 ;; 1/8	= 	0.125
 ;; 1/9	= 	0.(1)
 ;; 1/10	= 	0.1
-
+;;
 ;; Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It
 ;; can be seen that 1/7 has a 6-digit recurring cycle.
-
+;;
 ;; Find the value of d < 1000 for which 1/d contains the longest
 ;; recurring cycle in its decimal fraction part.
+;;
+;; notes ----------------------------------------------------------------------
+;;
+;; how to find the cycles
 
+(defn- decimal-raw
+  [d] (
+       iterate
+       (fn [{:keys [num] :as state}]
+         (cond (= num d)           {:res 1            :num num         :continue? false}
+               (< num d)           {:res nil          :num (* num 10)  :continue? true}
+               (zero? (rem num d)) {:res (quot num d) :num num         :continue? false}
+               :else               {:res (quot num d) :num (rem num d) :continue? true}))
+       {:num 1 :res 0 :continue? true}))
 
+(defn- decimal "returns a (lazy) seq of decimal of a 1/n"
+  [d]
+  (let
+      [s (decimal-raw d)
+       [a b] (split-with :continue? s)]
 
+    (->> a
+         (map :res)
+         (filter identity))))
 
+(comment "decimal" (->> 7
+                        decimal
+                        (take 25)))
